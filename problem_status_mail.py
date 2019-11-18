7323282fe0ae5a8e296d13db3523e166c864d2a9
Tom Kooij <tomkooij@tomkooij.nl>
2019-11-18 08:57:24 +0100

    Add scripts to send automatic mails using cron
    Daily: problem_status_mail.py (no data recieved yesterday)
    Mondays: down_status_mail.py (no data recieved >=week)

--- problem_status_mail.py
+++ problem_status_mail.py
@@ -3,6 +3,6 @@ import sys
 from send_status_mail import SendStatusMails
 
 message = open('problem_status_mail.txt').read()
-sm = SendStatusMails(trigger_on_status='problem', message=message)
+sm = SendStatusMails(trigger_on_status='problem', message=message, log_filename='/var/log/hisparc/status-mail.log')
 rc = sm.run()
 sys.exit(rc)
